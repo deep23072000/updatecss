c1e094d9f0130e2a04810cadc06995ebf0e9f9a0
Deepak patel <dp089901@gmail.com>
2024-07-13 12:45:36 +0530

    adding file

---
+++ index.js
@@ -0,0 +1,6 @@
+function run(){
+    document.documentElement.style.setProperty('--bg','white');
+    document.documentElement.style.setProperty('--col','black');
+    document.documentElement.style.setProperty('--btncol','red');
+    document.documentElement.style.setProperty('--btnbgcol','black');
+}
